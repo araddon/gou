4108c5e2652448086d2b015fa82ee4aa5f71df8d
Aaron Raddon <araddon@gmail.com>
2013-09-17 09:41:24 -0700

    Fix windows blow up

--- log.go
+++ log.go
@@ -247,6 +247,13 @@ const (
 // Determine is this process is running in a Terminal or not?
 func IsTerminal() bool {
 	ws := &winsize{}
+	isTerm := true
+	defer func() {
+		if r := recover(); r != nil {
+			isTerm = false
+		}
+	}()
+	// This blows up on windows
 	retCode, _, _ := syscall.Syscall(syscall.SYS_IOCTL,
 		uintptr(syscall.Stdin),
 		uintptr(_TIOCGWINSZ),
@@ -255,5 +262,5 @@ func IsTerminal() bool {
 	if int(retCode) == -1 {
 		return false
 	}
-	return true
+	return isTerm
 }
