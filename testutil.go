03b0f087d16116ea09f467e03f11951a3f443cf5
Aaron Raddon <araddon@gmail.com>
2013-10-27 08:44:06 -0700

    Delete events stuff

--- testutil.go
+++ testutil.go
@@ -20,6 +20,31 @@ var (
 	stopper  func()    = func() {}
 )
 
+// Wait for condition (defined by func) to be true
+// this is mostly for testing, but a utility to
+// create a ticker checking back every 100 ms to see
+// if something (the supplied check func) is done
+//
+//   WaitFor(func() bool {
+//      return ctr.Ct == 0
+//   },10)
+// timeout (in seconds) is the last arg
+func WaitFor(check func() bool, timeoutSecs int) {
+	timer := time.NewTicker(100 * time.Millisecond)
+	tryct := 0
+	for _ = range timer.C {
+		if check() {
+			timer.Stop()
+			break
+		}
+		if tryct >= timeoutSecs*10 {
+			timer.Stop()
+			break
+		}
+		tryct++
+	}
+}
+
 // Use this in combo with StopCheck() for test functions that must start
 // processes such as
 func SetStopper(f func()) {
